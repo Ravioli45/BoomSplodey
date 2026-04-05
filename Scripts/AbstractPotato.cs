68d49626a819373c08a392aadd9b923ffe99b303
Andrew R <Mydrewdog64@gmail.com>
2026-04-04 21:06:45 -0400

    Potato Bounces and Explodes

--- Scripts/AbstractPotato.cs
+++ Scripts/AbstractPotato.cs
@@ -7,36 +7,55 @@ public partial class AbstractPotato : RigidBody2D
 	public Vector2 Direction;
 
 	[Export]
-	public CollisionShape2D col;
+	public Area2D area;
 
-	public int collisionoff = 2;
 
 	[Export]
-	public int LaunchSpeed = 1500;
+	public int LaunchSpeed = 2000;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//ApplyImpulse(Vector2.Zero, Direction.Normalized() * LaunchSpeed);
+		
+		
 		LinearVelocity = Direction.Normalized() * LaunchSpeed;
+		GD.Print("Velocity: ", Direction);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (LinearVelocity.Length() > 0.1f)
-        {
-            Rotation = LinearVelocity.Angle();
-        }
-		if (collisionoff >= 0)
-		{
-			collisionoff--;
-		}
-		else
-		{
-			col.Disabled = false;
-		}
-
+	
 	}
 
+	public void Explode()
+	{
+		
+		 if(!Multiplayer.IsServer())return;
+
+        // Detect targets in explosion area
+        
+
+        foreach (var body in area.GetOverlappingBodies())
+        {
+			if(body is Player P){
+            GD.Print("Hit: " + P.Name);
+			//(Player)body.takedamage
+			}
+        }
+		QueueFree();
+	}
 	
+	public override void _IntegrateForces(PhysicsDirectBodyState2D state)
+	{
+
+		if (state.LinearVelocity.Length() > 0.1f)
+    	{
+        state.Transform = new Transform2D(state.LinearVelocity.Angle(), state.Transform.Origin);
+    	}
+
+    	if (state.GetContactCount() > 0)
+    	{
+        	LinearVelocity *= 0.8f;
+    	}
+		}
 }
